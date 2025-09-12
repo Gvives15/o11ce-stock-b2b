33ef9a76294eeddc22fd5bcf8a3828621f466cab
Gvives15 <germanvives15@gmail.com>
2025-09-12 03:07:25 -0300

    Add scaffolding and starter Django apps

--- config/config/settings.py
+++ config/config/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.core',
+    'apps.customers',
+    'apps.catalog',
+    'apps.stock',
+    'apps.orders',
+    'apps.notifications',
 ]
 
 MIDDLEWARE = [
