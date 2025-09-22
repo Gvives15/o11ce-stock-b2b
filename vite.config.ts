dd3252f43b288b84e1da4d40a7bc25b51f30f201
MGX <admin@mgx.dev>
2025-09-21 21:22:57 -0700

    Vite LAN configuration added.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
-
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
-    },
-  },
-})
+export default {
+  server: {
+    host: true,     // o "0.0.0.0"
+    port: 5173,
+    strictPort: true
+  }
+}
